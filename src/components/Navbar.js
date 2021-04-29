c0c620fa1935d2815e12813b9756e60799116b53
programmez <francoistonic@bbox.fr>
2021-04-29 12:00:58 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -61,10 +61,10 @@ const Navbar = class extends React.Component {
           >
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
-                About
+                A propos
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Ma vie, mon oeuvre
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
@@ -73,7 +73,7 @@ const Navbar = class extends React.Component {
                 Contact
               </Link>
               <Link className="navbar-item" to="/contact/examples">
-                Form Examples
+                Exemples
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
